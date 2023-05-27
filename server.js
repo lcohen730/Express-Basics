fb561a0916375f785802ef18eeb535b72bc790d1
lcohen730 <lcohen730@gmail.com>
2023-05-26 23:12:11 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -13,12 +13,24 @@ app.get('/greeting', (req, res) => {
 
 // Greetings to a Given Name
 app.get('/greeting/:name', (req, res) => {
-    res.send(`You know your way around a webpage, ${req.params.name}.`)
+    res.send(`<h2>You know your way around a webpage, ${req.params.name}.</h2>`)
 });
 
 // Tip Calculator
 app.get('/tip/:total/:percentage', (req, res) => {
-    res.send(`Tipping: It's the way of the Jedi....May the funds be with you. You should tip $${req.params.total * req.params.percentage / 100}.`)
+    res.send(`<h2>Tipping: It's the way of the Jedi....May the funds be with you. You should tip $${req.params.total * req.params.percentage / 100}.</h2>`)
+});
+
+// Magic 8 Ball
+app.get('/magic/:question', (req, res) => {
+    const responses = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.",
+    "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.","Yes.", "Signs point to yes.",
+     "Reply hazy; try again.", "Ask again later.","Better not tell you now.", "Cannot predict now.",
+      "Concentrate and ask again.","Don't count on it.", "My reply is no.", "My sources say no.",
+       "Outlook not so good.", "Very doubtful."];
+    const response = responses[Math.floor(Math.random()*responses.length)];
+    res.send(`<h2>${req.params.question}?</h2>
+    <h1>${response}</h1>`)
 });
 
 app.listen(3000, () => {
